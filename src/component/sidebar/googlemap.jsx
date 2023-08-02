63ce30a6e47b470ba2cf4e635fda563c69a20c66
Satyabrat Ojha <satyabratojha04@gmail.com>
2023-08-02 18:16:29 +0530

    contact info

--- src/component/sidebar/googlemap.jsx
+++ src/component/sidebar/googlemap.jsx
@@ -1,14 +1,12 @@
-
-
-
 const GoogleMap = () => {
-    return (
-        <div className="map-area">
-            <div className="maps">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.2275990948147!2d90.38698831543141!3d23.739261895117753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b85c740f17d1%3A0xdd3daab8c90eb11f!2sCodexCoder!5e0!3m2!1sen!2sbd!4v1607313671993!5m2!1sen!2sbd"></iframe>
-            </div>
-        </div>
-    );
-}
- 
+  return (
+    <div className="map-area">
+      <div className="maps">
+        {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.2275990948147!2d90.38698831543141!3d23.739261895117753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b85c740f17d1%3A0xdd3daab8c90eb11f!2sCodexCoder!5e0!3m2!1sen!2sbd!4v1607313671993!5m2!1sen!2sbd"></iframe> */}
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3500.3325820744467!2d77.34035327527269!3d28.67969628193781!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6cfd08be5b386bb%3A0x40f2f6420c576d62!2sYakeen%20The%20Learning%20Institute!5e0!3m2!1sen!2sin!4v1690806738426!5m2!1sen!2sin"></iframe>
+      </div>
+    </div>
+  );
+};
+
 export default GoogleMap;
