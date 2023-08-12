96eeae080a63135f002eba4c79c5fab517a83b10
arvind12suryawanshi <92415537+arvind12suryawanshi@users.noreply.github.com>
2023-08-12 22:35:14 +0530

    hi

--- src/component/section/Assistance/Assistance.jsx
+++ src/component/section/Assistance/Assistance.jsx
@@ -1,34 +1,85 @@
-import styles from "./Assistance.module.css";
-import jobAssistance from "./jobAssistance.png";
+// import styles from "./Assistance.module.css";
+// import jobAssistance from "./jobAssistance.png";
 
-const Assistance = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.imageContainer}>
-        <img className={styles.image} src={jobAssistance} alt="Left Image" />
-      </div>
-      <div className={styles.content}>
-        <h2>Yakeen The Learning Institute</h2>
-        <h1>Job Assistance Process</h1>
-        <div className={styles.container2}>
-          <div className={styles.row}>
-            <div className={styles.box}>1. Complete Course</div>
-            <div className={styles.box}>2. Submit Projects & Assignments</div>
-            <div className={styles.box}>3. Create Portfolio</div>
-          </div>
-          <div className={styles.row}>
-            <div className={styles.box}>
-              4. Get Shortlisted for Suitable Job
-            </div>
-            <div className={styles.box}>5. Attend Interview</div>
-          </div>
-          <div style={{ width: "100%" }}>
-            <div className={styles.placed}>6. Get Placed</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+// const Assistance = () => {
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.imageContainer}>
+//         <img className={styles.image} src={jobAssistance} alt="Left Image" />
+//       </div>
 
-export default Assistance;
+
+//       <div className={styles.content}>
+//         <h2>Yakeen The Learning Institute</h2>
+//         <h1>Job Assistance Process</h1>
+//         <div className={styles.container2}>
+//           <div className={styles.row}>
+//             <div className={styles.box}>1. Complete Course</div>
+//             <div className={styles.box}>2. Submit Projects & Assignments</div>
+//             <div className={styles.box}>3. Create Portfolio</div>
+//           </div>
+//           <div className={styles.row}>
+//             <div className={styles.box}>
+//               4. Get Shortlisted for Suitable Job
+//             </div>
+//             <div className={styles.box}>5. Attend Interview</div>
+//           </div>
+//           <div style={{ width: "100%" }}>
+//             <div className={styles.placed}>6. Get Placed</div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Assistance;
+
+
+// import React from "react";
+// import styles from "./Assistance.module.css";
+// import jobAssistance from "./jobAssistance.png";
+
+// const Assistance = () => {
+//   return (
+    
+//     <div className={styles.container}>
+//       <div className={styles.leftContainer}>
+//         <figure>
+//           <img className={styles.image} src={jobAssistance} alt="Left Image" />
+//         </figure>
+//       </div>
+//       <div className={styles.leftRightContainer}>
+//         <header>
+//           <h3>Yakeen The Learning Institute</h3>
+//           <h1>Job Assistance Process</h1>
+//         </header>
+//         <div className={styles.wrapper}>
+//           <div className={`${styles.box} ${styles.box1}`}>
+//             <div className={styles.num}>1.</div>
+//             <div className={styles.title}>Complete Course</div>
+//           </div>
+//           <div className={`${styles.box} ${styles.box2}`}>
+//             <div className={styles.num}>2.</div>
+//             <div className={styles.title}>Submit Projects & Assignments</div>
+//           </div>
+//           <div className={`${styles.box} ${styles.box3}`}>
+//             <div className={styles.num}>3.</div>
+//             <div className={styles.title}>Create Portfolio</div>
+//           </div>
+//           <div className={`${styles.box} ${styles.box4}`}>
+//             <div className={styles.num}>4.</div>
+//             <div className={styles.title}>Get Shortlisted for Suitable Job</div>
+//           </div>
+//           <div className={`${styles.box} ${styles.box5}`}>
+//             <div className={styles.num}>5.</div>
+//             <div className={styles.title}>Attend Interview</div>
+//           </div>
+//           <div className={`${styles.box} ${styles.box6}`}>6. Get Placed</div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Assistance;
