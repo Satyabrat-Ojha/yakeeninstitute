91e11af381f3d4cbc36977703ee809e726c0d804
Satyabrat Ojha <satyabratojha04@gmail.com>
2023-08-20 11:45:28 +0530

    admission id and minor nav

--- src/page/Admission/Admission.jsx
+++ src/page/Admission/Admission.jsx
@@ -41,7 +41,16 @@ const Admission = () => {
       course,
       education,
     };
-    const docRef = doc(db, "admissionUsers", email);
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+    const day = String(currentDate.getDate()).padStart(2, "0");
+    const hours = String(currentDate.getHours()).padStart(2, "0");
+    const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+    const seconds = String(currentDate.getSeconds()).padStart(2, "0");
+    const date = `${year}/${month}/${day} `;
+    const time = `${hours}:${minutes}:${seconds} `;
+    const docRef = doc(db, "admissionUsers", date + time + email);
     setDoc(docRef, data)
       .then(() => {
         alert("Document has been added successfully");
