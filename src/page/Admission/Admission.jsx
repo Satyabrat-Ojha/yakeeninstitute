e6976352a45620024a890015ab9b345dace0539e
Satyabrat Ojha <satyabratojha04@gmail.com>
2023-08-27 10:53:12 +0530

    payu link + firestore

--- src/page/Admission/Admission.jsx
+++ src/page/Admission/Admission.jsx
@@ -21,7 +21,12 @@ const Admission = () => {
   const [course, setCourse] = useState("");
   const [education, setEducation] = useState("");
 
-  const navigate = useNavigate();
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const day = String(currentDate.getDate()).padStart(2, "0");
+  const date = `${year}-${month}-${day} `;
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const data = {
@@ -40,21 +45,14 @@ const Admission = () => {
       gender,
       course,
       education,
+      registrationDate: date,
     };
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
-    const day = String(currentDate.getDate()).padStart(2, "0");
-    const hours = String(currentDate.getHours()).padStart(2, "0");
-    const minutes = String(currentDate.getMinutes()).padStart(2, "0");
-    const seconds = String(currentDate.getSeconds()).padStart(2, "0");
-    const date = `${year}/${month}/${day} `;
-    const time = `${hours}:${minutes}:${seconds} `;
-    const docRef = doc(db, "admissionUsers", date + time + email);
+    const random = Math.floor(Math.random() * 1000000000);
+    const docRef = doc(db, "admissionUsers", email + random);
     setDoc(docRef, data)
       .then(() => {
-        alert("Document has been added successfully");
-        navigate("https://pmny.in/pIrjJakiOaM0");
+        window.print();
+        window.location.replace("https://pmny.in/pIrjJakiOaM0");
       })
       .catch((error) => {
         console.log(error);
